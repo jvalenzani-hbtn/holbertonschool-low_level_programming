2600af89ca409c39d75646f05bf637cb9625d167
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-07-23 03:40:43 +0000

    Last 0 task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * Read: https://stackoverflow.com/questions/47257682/print-size-of-type-c
+*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
