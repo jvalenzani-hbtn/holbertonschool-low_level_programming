7e312e8740bd04c892551a5a8b51c20774a53864
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-09-14 19:54:25 +0000

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\"", 32);
