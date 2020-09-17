4e4973d252ca49f4e94ca442b48e5ab3b38be2ae
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-09-17 16:23:59 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,18 +6,20 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes there */
-    if (n == 0){
-        printf("%i is zero\n", n);
-    } else if (n > 0)
-        {
-           printf("%i is positive\n", n);
-        } else {
-printf("%i is negative\n", n);
-}
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	} else if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	} else
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
 }
