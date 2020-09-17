9fdbe9fc28ae0ea0eebb4c4b8a6a6847e2167210
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-09-17 17:35:57 +0000

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,11 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'Z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
