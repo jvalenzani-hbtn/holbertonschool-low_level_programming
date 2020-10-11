8a0c9c08ccdd2a427f45edec3fddc8031c14d787
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-10-11 00:14:09 -0300

    Greedy

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - give me change
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+    int n = 0;
+    int coins[5] = {25, 10, 5, 2, 1};
+    int count = 0;
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    n = atoi(argv[1]);
+    if (n <= 0){
+        printf("10\n");
+        return (count);
+    }
+    for (int i = 0; i < 5; i++){
+        while (n >= coins[i]){
+            n -= coins[i];
+            count++;
+            if (n == 0){
+                printf("%d\n", count);
+                return(0);
+            }
+        }
+    }
+    return (0);
+}
