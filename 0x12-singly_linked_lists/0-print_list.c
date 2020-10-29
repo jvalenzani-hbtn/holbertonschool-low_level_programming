f48415d17565243f5f96d7e8f02c5c9395533745
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-10-29 16:35:03 +0000

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+# include "lists.h"
+
+/**
+ * print_list - Prints a single linked list
+ * @h - list node 
+ *
+ * Return: Number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int nodeCount = 0;
+	if (h == NULL)
+	{	
+		return (nodeCount);
+	}
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		/*	printStr(h->str, h->len);*/
+		}
+		h = h->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
